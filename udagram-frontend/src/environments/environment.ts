6e73f526e512e0284b9eb988e51c12df7458a319
philong996 <tenbox1996@gmail.com>
2022-10-16 14:08:27 +0700

    update deployment to k8s

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'a25d67e181f5b4d888228bb00bf60bf7-1946704349.us-west-2.elb.amazonaws.com'
 };
 
 /*
